1c51277a32c8bd1ca50078c559d01db4ffb8fdd9
Kumudikatest <kumudikarupasinghe+adrt@gmail.com>
2018-11-26 15:50:43 +0530

    change repo

---
+++ index.js
@@ -0,0 +1,4 @@
+// This file is used to register all your cloud functions in GCP.
+// DO NOT EDIT/DELETE THIS, UNLESS YOU KNOW WHAT YOU ARE DOING.
+
+exports.Testfunction = require("./Test/function.js").handler;
